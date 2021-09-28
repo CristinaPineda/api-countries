7ebf2a3b7c9cef3d6556bbcc1697ebaa6a18f54e
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-28 00:26:05 -0300

    cria api rest express core

---
+++ src/Controllers/UsuarioController.js
@@ -0,0 +1,24 @@
+const raw = require('../Services/contriesJson.json')
+
+exports.post = (req, res, next) => {
+    res.status(201).send('Rota POST!');
+ };
+  
+ exports.put = (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send(`Rota PUT com ID! --> ${id}`);
+ };
+  
+ exports.delete = (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send(`Rota DELETE com ID! --> ${id}`);
+ };
+  
+ exports.get = (req, res, next) => {
+    res.status(200).send( raw );
+ };
+  
+ exports.getById = (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send(`Rota GET com ID! ${id}`);
+ };
---
+++ src/Routes/UsuarioRoute.js
@@ -0,0 +1,8 @@
+const UsuarioController = require('../Controllers/UsuarioController');
+module.exports = (app) => {
+   app.post('/countries', UsuarioController.post);
+   app.put('/countries/:id', UsuarioController.put);
+   app.delete('/countries/:id', UsuarioController.delete);
+   app.get('/countries', UsuarioController.get);
+   app.get('/countries/:id', UsuarioController.getById);
+}
